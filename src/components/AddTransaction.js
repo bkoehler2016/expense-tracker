7266afa490f02efde6832934d1515cb8acdc8322
Ben Koehler <bkoehler85282@gmail.com>
2020-02-22 15:11:36 -0700

    added contextapi to components

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    };
+
+    addTransaction(newTransaction);
+  };
+
   return (
     <>
-      <h3>Add New transaction</h3>
+      <h3>Add new transaction</h3>
       <form onSubmit={onSubmit}>
         <div className="form-control">
           <label htmlFor="text">Text</label>
