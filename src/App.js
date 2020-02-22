c74bee8260046f56eeeca73e4ae06178f9cce91e
Ben Koehler <bkoehler85282@gmail.com>
2020-02-22 14:59:35 -0700

    built out balance component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 
 import "./App.css";
 import { Header } from "./components/Header";
+import { Balance } from "./components/Balance";
 
 function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </div>
   );
 }
